13fc6a8b2c9729a3b4b4a39f689391b8eefd4c9e
Nishanthan Ravichandran <nishanthravi226@gmail.com>
2023-10-07 01:49:59 -0400

    Initial project setup

---
+++ src/com/caffeine/appl/Caffeine.java
@@ -0,0 +1,16 @@
+package com.caffeine.appl;
+
+/**
+ * This class is the Main application class of this project
+ */
+public class Caffeine {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome Everyone, ACC Project Codebase");
+	}
+
+}
