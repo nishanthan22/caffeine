dbda72cbabb91bbe45a2316e00990f1e4f609d66
AasthaKiri <98647483+AasthaKiri@users.noreply.github.com>
2023-11-22 04:08:29 -0500

    methods

--- src/com/caffeine/manager/Utilities.java
+++ src/com/caffeine/manager/Utilities.java
@@ -1,6 +1,9 @@
 package com.caffeine.manager;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * This class consists of all the necessary and common non-featural methods that
@@ -18,5 +21,17 @@ public class Utilities {
 		String finalFileName = path.concat(File.separator).concat(fileName);
 		return finalFileName;
 	}
+	
+	
+	public static void writeConsoleToFile(String filePath, String consoleOutput) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            bw.write(consoleOutput);
+            System.out.println("Console output saved to file: " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
 
 }
