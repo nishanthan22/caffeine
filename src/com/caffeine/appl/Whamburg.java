34aefb18d564f20b88679c955a99e1a3f427266d
AasthaKiri <98647483+AasthaKiri@users.noreply.github.com>
2023-11-22 04:06:38 -0500

    conversion in text and csv

--- src/com/caffeine/appl/Whamburg.java
+++ src/com/caffeine/appl/Whamburg.java
@@ -16,23 +16,19 @@ import com.caffeine.manager.Utilities;
 public class Whamburg {
 	public static void main(String[] args) {
 
-		String filePath = Utilities.getFilePath(Constants.FILE_NAME_PATH_PREFIX, "Whamburg.txt");
+		WebDriver driver = new ChromeDriver();
 
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+		// Navigate to the desired webpage
+		driver.get(Constants.WHAMBURG_URL);
+		
+		String txtFilePath = Utilities.getFilePath(Constants.FILE_NAME_PATH_PREFIX, "Whamburg.txt");
+		String csvFilePath = Utilities.getFilePath(Constants.FILE_NAME_PATH_PREFIX, "Whamburg.csv");
+		
+	    StringBuilder consoleOutput = new StringBuilder();
+	    StringBuilder csvOutput = new StringBuilder();
+	       csvOutput.append("Title,Price,Description\n");
 
-			// Heading: BURGER & SANDWICH COMBOS
-//	            bw.write("Heading: BURGER & SANDWICH COMBOS");
-//	            bw.newLine();
-
-			WebDriver driver = new ChromeDriver();
-
-			// Navigate to the desired webpage
-			driver.get(Constants.WHAMBURG_URL);
-
-//	    		System.out.println("Heading : BURGER & SANDWICH COMBOS");
-			bw.write("Heading: BURGER & SANDWICH COMBOS");
-			bw.newLine();
-			bw.newLine();
+			consoleOutput.append("BURGER & SANDWICH COMBOS \n--------------------\n");
 
 			WebElement title = driver.findElement(By.cssSelector("[data-id='0ee3640'] .elementor-widget-container"));
 
@@ -43,7 +39,7 @@ public class Whamburg {
 			List<WebElement> thirdDiv = title.findElements(
 					By.cssSelector("div[data-id='793348c'] div[data-id='57c4447'] div[data-id='46dc225'] "));
 
-			Map<String, String> dataMap = new HashMap<>();
+//			Map<String, String> dataMap = new HashMap<>();
 
 			for (int i = 0; i < firstDiv.size(); i++) {
 				WebElement titleElement = firstDiv.get(i).findElement(By.cssSelector("h3 a"));
@@ -51,33 +47,22 @@ public class Whamburg {
 
 				WebElement descElement = secondDiv.get(i).findElement(By.cssSelector("p"));
 				String descText = descElement.getText();
-
+				
 				WebElement priceElement = thirdDiv.get(i).findElement(By.cssSelector("span"));
 				String priceText = priceElement.getText();
+				
+				String descriptionCSV = descElement.getText().replace(",", ";");
+	            csvOutput.append(String.format("%s,%s,%s\n", titleText, priceText, descriptionCSV));
 
-				// Combine description and price into a single string
-				String descAndPrice = descText + ",  \nPrice: " + priceText;
-
-				// Add to the map with title as key and description & price as value
-				dataMap.put(titleText, descAndPrice);
+				consoleOutput.append(Constants.TITLE).append(titleText).append("\n")
+                .append(Constants.PRICE).append(priceText).append("\n")
+                .append(Constants.DESCRIPTION).append(descText).append("\n")
+                .append("----------\n");
 			}
+			
+			consoleOutput.append("BURGER & SANDWICH \n--------------------\n");
 
-			// ... Your existing code that fetches data and prints to console ...
-			// Example: Print to console and write to file within the loop
-
-			for (Map.Entry<String, String> entry : dataMap.entrySet()) {
-				bw.write(Constants.TITLE + entry.getKey());
-				bw.newLine();
-				bw.write(Constants.DESCRIPTION + entry.getValue());
-				bw.newLine();
-				bw.write("----------------------------");
-				bw.newLine();
-			}
-
-			bw.write("Heading : BURGER & SANDWICH ");
-			bw.newLine();
-			bw.newLine();
-
+//
 			WebElement burgAndSand = driver
 					.findElement(By.cssSelector("[data-id='633d720'] .elementor-widget-container"));
 
@@ -88,7 +73,6 @@ public class Whamburg {
 			List<WebElement> bsThirdDiv = burgAndSand.findElements(
 					By.cssSelector("div[data-id='793348c'] div[data-id='57c4447'] div[data-id='46dc225'] "));
 
-			Map<String, String[]> bsDataMap = new HashMap<>();
 
 			for (int i = 0; i < bsFirst.size(); i++) {
 				WebElement bsTitle = bsFirst.get(i).findElement(By.cssSelector("h3 a"));
@@ -99,29 +83,19 @@ public class Whamburg {
 
 				WebElement bsProductPrice = bsThirdDiv.get(i).findElement(By.cssSelector("span"));
 				String bsPriceText = bsProductPrice.getText();
+				
+				String descriptionCSV = bsDesc.getText().replace(",", ";");
+	            csvOutput.append(String.format("%s,%s,%s\n", bsTitleText, bsPriceText, descriptionCSV));
 
 				// Create an array to hold description and price
-				String[] bsDescAndPrice = { bsDescText, bsPriceText };
-
-				// Add to the map with title as key and description & price array as value
-				bsDataMap.put(bsTitleText, bsDescAndPrice);
+				consoleOutput.append(Constants.TITLE).append(bsTitleText).append("\n")
+                .append(Constants.PRICE).append(bsPriceText).append("\n")
+                .append(Constants.DESCRIPTION).append(bsDescText).append("\n")
+                .append("----------\n");
 			}
 
-			// Printing the map content
-			for (Map.Entry<String, String[]> entry : bsDataMap.entrySet()) {
-				bw.write(Constants.TITLE + entry.getKey());
-				bw.newLine();
-				bw.write(Constants.DESCRIPTION + entry.getValue()[0]);
-				bw.newLine();
-				bw.write(Constants.PRICE + entry.getValue()[1]);
-				bw.newLine();
-				bw.write("----------------------------");
-				bw.newLine();
-			}
-
-			bw.write("Heading : FRIES ");
-			bw.newLine();
-			bw.newLine();
+		
+			consoleOutput.append("FRIES\n--------------------\n");
 
 			WebElement fries = driver.findElement(By.cssSelector("[data-id='825e047'] .elementor-widget-container"));
 
@@ -132,7 +106,6 @@ public class Whamburg {
 			List<WebElement> friesThirdDiv = fries.findElements(
 					By.cssSelector("div[data-id='793348c'] div[data-id='57c4447'] div[data-id='46dc225'] "));
 
-			Map<String, String[]> friesDataMap = new HashMap<>();
 
 			for (int i = 0; i < friesFirst.size(); i++) {
 				WebElement friesTitle = friesFirst.get(i).findElement(By.cssSelector("h3 a"));
@@ -143,30 +116,20 @@ public class Whamburg {
 
 				WebElement friesProductPrice = friesThirdDiv.get(i).findElement(By.cssSelector("span"));
 				String friesPriceText = friesProductPrice.getText();
+				
+				String descriptionCSV = friesDesc.getText().replace(",", ";");
+	            csvOutput.append(String.format("%s,%s,%s\n", friesTitleText, friesPriceText, descriptionCSV));
+				
+				consoleOutput.append(Constants.TITLE).append(friesTitleText).append("\n")
+                .append(Constants.PRICE).append(friesPriceText).append("\n")
+                .append(Constants.DESCRIPTION).append(friesDescText).append("\n")
+                .append("----------\n");
 
-				// Create an array to hold description and price
-				String[] friesDescAndPrice = { friesDescText, friesPriceText };
-
-				// Add to the map with title as key and description & price array as value
-				friesDataMap.put(friesTitleText, friesDescAndPrice);
 			}
 
-			// Printing the map content
-			for (Map.Entry<String, String[]> entry : friesDataMap.entrySet()) {
-				bw.write(Constants.TITLE + entry.getKey());
-				bw.newLine();
-				bw.write(Constants.DESCRIPTION + entry.getValue()[0]);
-				bw.newLine();
-				bw.write(Constants.PRICE + entry.getValue()[1]);
-				bw.newLine();
-				bw.write("----------------------------");
-				bw.newLine();
-//	    		    System.out.println("----------------------------");
-			}
+			
+			consoleOutput.append("DESSERT\n--------------------\n");
 
-			bw.write("Heading : DESSERT ");
-			bw.newLine();
-			bw.newLine();
 
 			WebElement dessert = driver.findElement(By.cssSelector("[data-id='95452fb'] .elementor-widget-container"));
 
@@ -177,7 +140,6 @@ public class Whamburg {
 			List<WebElement> dessertThirdDiv = dessert.findElements(
 					By.cssSelector("div[data-id='793348c'] div[data-id='57c4447'] div[data-id='46dc225'] "));
 
-			Map<String, String[]> dessertDataMap = new HashMap<>();
 
 			for (int i = 0; i < dessertFirst.size(); i++) {
 				WebElement dessertTitle = dessertFirst.get(i).findElement(By.cssSelector("h3 a"));
@@ -188,29 +150,20 @@ public class Whamburg {
 
 				WebElement dessertProductPrice = dessertThirdDiv.get(i).findElement(By.cssSelector("span"));
 				String dessertPriceText = dessertProductPrice.getText();
+				
+				String descriptionCSV = dessertDesc.getText().replace(",", ";");
+	            csvOutput.append(String.format("%s,%s,%s\n", dessertTitleText, dessertPriceText, descriptionCSV));
 
 				// Create an array to hold description and price
-				String[] dessertDescAndPrice = { dessertDescText, dessertPriceText };
-
-				// Add to the map with title as key and description & price array as value
-				dessertDataMap.put(dessertTitleText, dessertDescAndPrice);
+				consoleOutput.append(Constants.TITLE).append(dessertTitleText).append("\n")
+                .append(Constants.PRICE).append(dessertPriceText).append("\n")
+                .append(Constants.DESCRIPTION).append(dessertDescText).append("\n")
+                .append("----------\n");
 			}
+			
+	
+			consoleOutput.append("BEVERAGES\n--------------------\n");
 
-			// Printing the map content
-			for (Map.Entry<String, String[]> entry : dessertDataMap.entrySet()) {
-				bw.write(Constants.TITLE + entry.getKey());
-				bw.newLine();
-				bw.write(Constants.DESCRIPTION + entry.getValue()[0]);
-				bw.newLine();
-				bw.write(Constants.PRICE + entry.getValue()[1]);
-				bw.newLine();
-				bw.write("----------------------------");
-				bw.newLine();
-			}
-
-			bw.write("Heading : BEVERAGES ");
-			bw.newLine();
-			bw.newLine();
 
 			WebElement beverages = driver
 					.findElement(By.cssSelector("[data-id='03aee40'] .elementor-widget-container"));
@@ -222,7 +175,6 @@ public class Whamburg {
 			List<WebElement> beveragesThirdDiv = beverages.findElements(
 					By.cssSelector("div[data-id='793348c'] div[data-id='57c4447'] div[data-id='46dc225'] "));
 
-			Map<String, String[]> beveragesDataMap = new HashMap<>();
 
 			for (int i = 0; i < beveragesFirst.size(); i++) {
 				WebElement beveragesTitle = beveragesFirst.get(i).findElement(By.cssSelector("h3 a"));
@@ -238,29 +190,21 @@ public class Whamburg {
 
 				WebElement beveragesProductPrice = beveragesThirdDiv.get(i).findElement(By.cssSelector("span"));
 				String beveragesPriceText = beveragesProductPrice.getText();
+				
+//				String descriptionCSV = beveragesDescText.replace(",", ";");
+	            csvOutput.append(String.format("%s,%s\n", beveragesTitleText, beveragesPriceText));
 
-				// Create an array to hold description and price
-				String[] beveragesDescAndPrice = { beveragesDescText, beveragesPriceText };
-
-				// Add to the map with title as key and description & price array as value
-				beveragesDataMap.put(beveragesTitleText, beveragesDescAndPrice);
+				consoleOutput.append(Constants.TITLE).append(beveragesTitleText).append("\n")
+                .append(Constants.PRICE).append(beveragesPriceText).append("\n")
+                .append(Constants.DESCRIPTION).append(beveragesDescText).append("\n")
+                .append("----------\n");
 			}
 
 			// Printing the map content
-			for (Map.Entry<String, String[]> entry : beveragesDataMap.entrySet()) {
-				bw.write(Constants.TITLE + entry.getKey());
-				bw.newLine();
-				bw.write(Constants.DESCRIPTION + entry.getValue()[0]);
-				bw.newLine();
-				bw.write(Constants.PRICE + entry.getValue()[1]);
-				bw.newLine();
-				bw.write("----------------------------");
-				bw.newLine();
-			}
+			
 
-			bw.write("Heading : MILKSHAKES ");
-			bw.newLine();
-			bw.newLine();
+			consoleOutput.append("MILKSHAKES\n--------------------\n");
+
 
 			WebElement shakes = driver.findElement(By.cssSelector("[data-id='00d6329'] .elementor-widget-container"));
 
@@ -271,7 +215,6 @@ public class Whamburg {
 			List<WebElement> shakesThirdDiv = shakes.findElements(
 					By.cssSelector("div[data-id='793348c'] div[data-id='57c4447'] div[data-id='46dc225'] "));
 
-			Map<String, String[]> shakesDataMap = new HashMap<>();
 
 			for (int i = 0; i < shakesFirst.size(); i++) {
 				WebElement shakesTitle = shakesFirst.get(i).findElement(By.cssSelector("h3 a"));
@@ -282,38 +225,23 @@ public class Whamburg {
 
 				WebElement shakesProductPrice = shakesThirdDiv.get(i).findElement(By.cssSelector("span"));
 				String shakesPriceText = shakesProductPrice.getText();
+				
+				String descriptionCSV = shakesDesc.getText().replace(",", ";");
+	            csvOutput.append(String.format("%s,%s\n", shakesTitleText, shakesPriceText, descriptionCSV));
 
-				// Create an array to hold description and price
-				String[] shakesDescAndPrice = { shakesDescText, shakesPriceText };
-
-				// Add to the map with title as key and description & price array as value
-				shakesDataMap.put(shakesTitleText, shakesDescAndPrice);
+				consoleOutput.append(Constants.TITLE).append(shakesTitleText).append("\n")
+                .append(Constants.PRICE).append(shakesPriceText).append("\n")
+                .append(Constants.DESCRIPTION).append(shakesDescText).append("\n")
+                .append("----------\n");
+				
 			}
 
-			// Printing the map content
-			for (Map.Entry<String, String[]> entry : shakesDataMap.entrySet()) {
-				bw.write(Constants.TITLE + entry.getKey());
-				bw.newLine();
-				bw.write(Constants.DESCRIPTION + entry.getValue()[0]);
-				bw.newLine();
-				bw.write(Constants.PRICE + entry.getValue()[1]);
-				bw.newLine();
-				bw.write("----------------------------");
-				bw.newLine();
-			}
-
-			bw.write("Heading : SIDES ");
-			bw.newLine();
-			bw.newLine();
+			consoleOutput.append("SIDES\n--------------------\n");
 
 			WebElement sides = driver.findElement(By.cssSelector("[data-id='e9082b7'] .elementor-widget-container"));
 
-			List<WebElement> sidesFirst = sides.findElements(
-					By.cssSelector("div[data-id='793348c'] div[data-id='57c4447'] div[data-id='bdbe0f6'] "));
-			List<WebElement> sidesThirdDiv = sides.findElements(
-					By.cssSelector("div[data-id='793348c'] div[data-id='57c4447'] div[data-id='46dc225'] "));
-
-			Map<String, String> sidesDataMap = new HashMap<>();
+			List<WebElement> sidesFirst = sides.findElements(By.cssSelector("div[data-id='793348c'] div[data-id='57c4447'] div[data-id='bdbe0f6'] "));
+			List<WebElement> sidesThirdDiv = sides.findElements(By.cssSelector("div[data-id='793348c'] div[data-id='57c4447'] div[data-id='46dc225'] "));
 
 			for (int i = 0; i < sidesFirst.size(); i++) {
 				WebElement sidesTitle = sidesFirst.get(i).findElement(By.cssSelector("h3 a"));
@@ -321,25 +249,19 @@ public class Whamburg {
 
 				WebElement sidesProductPrice = sidesThirdDiv.get(i).findElement(By.cssSelector("span"));
 				String sidesPriceText = sidesProductPrice.getText();
+				
+	            csvOutput.append(String.format("%s,%s\n", sidesTitleText, sidesPriceText));
 
-				// Add to the map with title as key and price as value
-				sidesDataMap.put(sidesTitleText, sidesPriceText);
+				consoleOutput.append(Constants.TITLE).append(sidesTitleText).append("\n")
+                .append(Constants.PRICE).append(sidesPriceText).append("\n")
+                .append("----------\n");
 			}
 
 			// Printing the map content
-			for (Map.Entry<String, String> entry : sidesDataMap.entrySet()) {
-				bw.write(Constants.TITLE + entry.getKey());
-				bw.newLine();
-				bw.write(Constants.PRICE + entry.getValue());
-				bw.newLine();
-				bw.write("----------------------------");
-				bw.newLine();
+			
 
-			}
+			consoleOutput.append("BEER\n--------------------\n");
 
-			bw.write("Heading : BEER ");
-			bw.newLine();
-			bw.newLine();
 
 			WebElement beer = driver.findElement(By.cssSelector("[data-id='6fce461'] .elementor-widget-container"));
 
@@ -348,7 +270,6 @@ public class Whamburg {
 			List<WebElement> beerThirdDiv = beer.findElements(
 					By.cssSelector("div[data-id='793348c'] div[data-id='57c4447'] div[data-id='46dc225'] "));
 
-			Map<String, String> beerDataMap = new HashMap<>();
 
 			for (int i = 0; i < beerFirst.size(); i++) {
 				WebElement beerTitle = beerFirst.get(i).findElement(By.cssSelector("h3 a"));
@@ -357,27 +278,18 @@ public class Whamburg {
 				WebElement beerProductPrice = beerThirdDiv.get(i).findElement(By.cssSelector("span"));
 				String beerPriceText = beerProductPrice.getText();
 
-				// Add to the map with title as key and price as value
-				beerDataMap.put(beerTitleText, beerPriceText);
+	            csvOutput.append(String.format("%s,%s\n", beerTitleText, beerPriceText));
+
+				consoleOutput.append(Constants.TITLE).append(beerTitleText).append("\n")
+                .append(Constants.PRICE).append(beerPriceText).append("\n")
+                .append("----------\n");
 			}
 
-			// Printing the map content
-			for (Map.Entry<String, String> entry : beerDataMap.entrySet()) {
-				bw.write(Constants.TITLE + entry.getKey());
-				bw.newLine();
-				bw.write(Constants.PRICE + entry.getValue());
-				bw.newLine();
-				bw.write("----------------------------");
-				bw.newLine();
-			}
-
-			System.out.println("Data saved to file: " + filePath); 
 			driver.quit();
 
-		} catch (Exception e) {
-			e.printStackTrace();
 
-			// Close the browser
+			 Utilities.writeConsoleToFile(csvFilePath, csvOutput.toString());
+
+			   Utilities.writeConsoleToFile(txtFilePath, consoleOutput.toString());
 		}
 	}
-}
