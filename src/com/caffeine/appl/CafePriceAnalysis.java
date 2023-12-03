9b36aaf55a1bce279b9c3ed2c38cfbf524cda834
Rakul Vimala Manokaran <rakulcareer6@gmail.com>
2023-12-02 20:21:34 -0500

    Integration changes for option 2&3 final

--- src/com/caffeine/appl/CafePriceAnalysis.java
+++ src/com/caffeine/appl/CafePriceAnalysis.java
@@ -1,197 +1,211 @@
 package com.caffeine.appl;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale.Category;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.caffeine.manager.Features;
 import com.caffeine.manager.Utilities;
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import com.opencsv.exceptions.CsvValidationException;
 
-class TrieNode {
-	Map<Character, TrieNode> children;
-	boolean isEndOfWord;
 
-	TrieNode() {
-		this.children = new HashMap<>();
-		this.isEndOfWord = false;
-	}
+class TrieNode {
+    Map<Character, TrieNode> children;
+    boolean isEndOfWord;
+
+    TrieNode() {
+        this.children = new HashMap<>();
+        this.isEndOfWord = false;
+    }
 }
 
 class Trie {
-	TrieNode root;
+    TrieNode root;
 
-	Trie() {
-		this.root = new TrieNode();
-	}
+    Trie() {
+        this.root = new TrieNode();
+    }
 
-	void insert(String word) {
-		TrieNode node = root;
-		for (char ch : word.toCharArray()) {
-			node.children.putIfAbsent(ch, new TrieNode());
-			node = node.children.get(ch);
-		}
-		node.isEndOfWord = true;
-	}
+    void insert(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            node.children.putIfAbsent(ch, new TrieNode());
+            node = node.children.get(ch);
+        }
+        node.isEndOfWord = true;
+    }
 
-	List<String> searchCompletions(String partialWord) {
-		List<String> completions = new ArrayList<>();
-		TrieNode node = root;
+    List<String> searchCompletions(String partialWord) {
+        List<String> completions = new ArrayList<>();
+        TrieNode node = root;
 
-		for (char ch : partialWord.toCharArray()) {
-			if (node.children.containsKey(ch)) {
-				node = node.children.get(ch);
-			} else {
-				return completions;
-			}
-		}
+        for (char ch : partialWord.toCharArray()) {
+            if (node.children.containsKey(ch)) {
+                node = node.children.get(ch);
+            } else {
+                return completions;
+            }
+        }
 
-		collectCompletions(node, partialWord, completions);
-		return completions;
-	}
+        collectCompletions(node, partialWord, completions);
+        return completions;
+    }
 
-	private void collectCompletions(TrieNode node, String prefix, List<String> completions) {
-		if (node.isEndOfWord) {
-			completions.add(prefix);
-		}
+    private void collectCompletions(TrieNode node, String prefix, List<String> completions) {
+        if (node.isEndOfWord) {
+            completions.add(prefix);
+        }
 
-		for (char ch : node.children.keySet()) {
-			collectCompletions(node.children.get(ch), prefix + ch, completions);
-		}
-	}
+        for (char ch : node.children.keySet()) {
+            collectCompletions(node.children.get(ch), prefix + ch, completions);
+        }
+    }
 }
 
 class CafeCategory {
-	private String name;
-	private double price;
+    private String name;
+    private double price;
 
-	public CafeCategory(String name, double price) {
-		this.name = name;
-		this.price = price;
-	}
+    public CafeCategory(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public double getPrice() {
-		return price;
-	}
+    public double getPrice() {
+        return price;
+    }
 }
 
 class Cafe {
-	private Map<String, List<CafeCategory>> menu;
+    private Map<String, List<CafeCategory>> menu;
 
-	public Cafe() {
-		this.menu = new HashMap<>();
-	}
+    public Cafe() {
+        this.menu = new HashMap<>();
+    }
 
-	public void addCategory(String name, double price) {
-		menu.computeIfAbsent(name.toLowerCase(), k -> new ArrayList<>()).add(new CafeCategory(name, price));
-	}
+    public void addCategory(String name, double price) {
+        menu.computeIfAbsent(name.toLowerCase(), k -> new ArrayList<>())
+            .add(new CafeCategory(name, price));
+    }
 
-	public List<CafeCategory> getCategories(String name) {
-		return menu.getOrDefault(name.toLowerCase(), Collections.emptyList());
-	}
+    public List<CafeCategory> getCategories(String name) {
+        return menu.getOrDefault(name.toLowerCase(), Collections.emptyList());
+    }
 }
 
 public class CafePriceAnalysis {
-	private Trie wordCompletionTrie;
-	private Map<String, CafeCategory> invertedIndex;
-	private Map<String, Cafe> cafes;
+    private Trie wordCompletionTrie;
+    private Map<String, CafeCategory> invertedIndex;
+    private Map<String, Cafe> cafes;
 
-	public CafePriceAnalysis() {
-		wordCompletionTrie = new Trie();
-		invertedIndex = new HashMap<>();
-		cafes = new HashMap<>();
-		initializeDataFromCSV(Utilities.getFilePath(Constants.FILE_NAME_PATH_PREFIX,
-				Constants.COMPARABLE_FILE.concat(Constants.CSV_EXTENSION), true));
-	}
+    public CafePriceAnalysis() {
+        wordCompletionTrie = new Trie();
+        invertedIndex = new HashMap<>();
+        cafes = new HashMap<>();
+        initializeDataFromCSV(Utilities.getFilePath(Constants.FILE_NAME_PATH_PREFIX, Constants.COMPARABLE_FILE.concat(Constants.CSV_EXTENSION), true)); 
+    }
 
-	private void initializeDataFromCSV(String fileName) {
-		try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
-			String[] line;
-			while ((line = reader.readNext()) != null) {
-				String cafeName = line[0].trim();
-				String foodOption = line[1].trim();
-				try {
-					double price = Double.parseDouble(line[2].trim()); 
-					wordCompletionTrie.insert(foodOption);
-					invertedIndex.put(foodOption.toLowerCase(), new CafeCategory(foodOption, price)); 
-					cafes.computeIfAbsent(cafeName, k -> new Cafe()).addCategory(foodOption, price);
-				} catch (NumberFormatException e) {
-					System.err.println("Invalid price format for " + foodOption + " in cafe " + cafeName);
-				}
+    private void initializeDataFromCSV(String fileName) {
+        try  {
+        CSVReader reader = new CSVReader(new FileReader(fileName));
+    	String[] line;
+        while ((line = reader.readNext()) != null) {
+        		String cafeName = line[0].trim();
+                String foodOption = line[1].trim();
+                try {
+                    double price = Double.parseDouble(line[2].trim());
 
-			}
-		} catch (IOException | CsvValidationException e) {
-			e.printStackTrace();
-		}
-	}
+                    wordCompletionTrie.insert(foodOption);
+                    invertedIndex.put(foodOption.toLowerCase(), new CafeCategory(foodOption, price));
 
-	public List<String> wordCompletion(String partialWord) {
-		return wordCompletionTrie.searchCompletions(partialWord.toLowerCase());
-	}
+                    cafes.computeIfAbsent(cafeName, k -> new Cafe()).addCategory(foodOption, price);
+                } catch (NumberFormatException e) {
+                    System.err.println("Invalid price format for " + foodOption + " in cafe " + cafeName);
+                }
+        	
+        }
+    } catch (IOException | CsvValidationException e) {
+        e.printStackTrace();
+    }
+    }
 
-	public void displayCategoriesWithPrices(String userInput) {
-		String lowerCaseInput = userInput.toLowerCase();
+    public List<String> wordCompletion(String partialWord) {
+        return wordCompletionTrie.searchCompletions(partialWord.toLowerCase());
+    }
 
-		List<CafeCategory> categories = invertedIndex.values().stream()
-				.filter(cat -> cat.getName().toLowerCase().contains(lowerCaseInput)).collect(Collectors.toList());
+    public void displayCategoriesWithPrices(String userInput) {
+        String lowerCaseInput = userInput.toLowerCase();
 
-		if (!categories.isEmpty()) {
-			System.out.println("\nDid you mean: "
-					+ categories.stream().map(CafeCategory::getName).collect(Collectors.toList()) + "\n");
-			categories.forEach(category -> {
-				findLowestPrice(category.getName());
-			});
-		} else {
-			Features.spellChecker(userInput);
-		}
-	}
+        List<CafeCategory> matchingCategories = invertedIndex.values().stream()
+                .filter(cat -> cat.getName().toLowerCase().contains(lowerCaseInput))
+                .collect(Collectors.toList());
 
-	public void findLowestPrice(String dishName) {
-		List<String> cafesWithCategory = new ArrayList<>();
+        if (!matchingCategories.isEmpty()) {
+            System.out.println("\nDid you mean: " + matchingCategories.stream().map(CafeCategory::getName).collect(Collectors.toList()) + "\n");
 
-		cafes.forEach((cafeName, cafe) -> {
-			cafe.getCategories(dishName).forEach(cafeCategory -> {
-				System.out.println("Category: " + cafeCategory.getName() + ", Price: " + cafeCategory.getPrice()
-						+ " from " + cafeName);
-				cafesWithCategory.add(cafeName);
-			});
-		});
+            matchingCategories.forEach(cat -> {
+                System.out.println(cat.getName() + " priced " + cat.getPrice() + " from " + findCafeNameForCategory(cat.getName()));
+            });
 
-		if (!cafesWithCategory.isEmpty()) {
-			double lowestPrice = cafesWithCategory.stream().map(cafeKey -> cafes.get(cafeKey).getCategories(dishName))
-					.flatMap(List::stream).mapToDouble(CafeCategory::getPrice).min().orElse(Double.MAX_VALUE);
+            double lowestPrice = matchingCategories.stream()
+                    .mapToDouble(CafeCategory::getPrice)
+                    .min()
+                    .orElse(Double.MAX_VALUE);
 
-			List<String> cafesWithLowestPrice = cafesWithCategory.stream().filter(cafeKey -> cafes.get(cafeKey)
-					.getCategories(dishName).stream().anyMatch(category -> category.getPrice() == lowestPrice))
-					.toList();
+            List<String> cafesWithLowestPrice = matchingCategories.stream()
+                    .filter(cat -> cat.getPrice() == lowestPrice)
+                    .map(cat -> {
+                        String cafeName = findCafeNameForCategory(cat.getName());
+                        return "Here's the Best Deal for you: " + cat.getName() + " priced " + lowestPrice + " from " + cafeName;
+                    })
+                    .collect(Collectors.toList());
 
-			System.out.println("\nHere's the Best Deal for you for " + dishName + ": " + lowestPrice + " from "
-					+ cafesWithLowestPrice + "\n");
-		} else {
-			System.out.println("No prices found for " + dishName);
-		}
+            if (!cafesWithLowestPrice.isEmpty()) {
+                cafesWithLowestPrice.forEach(System.out::println);
+                System.out.println();
+            } else {
+                System.out.println("No prices found for " + userInput);
+            }
+        } else {
+            System.out.println("No results found for " + userInput);
+        }
+    }
 
-	}
-
-	public void processUserInput(String userInput) {
-		if (userInput != null && !userInput.isEmpty()) {
-			if (Features.validateInput(userInput))
-				displayCategoriesWithPrices(userInput);
-			else {
-				System.out.println("Invalid input. Please enter letters and spaces only.");
-			}
-		} else {
-			System.out.println("Input cannot be empty.");
-		}
-	}
+    private String findCafeNameForCategory(String categoryName) {
+        return cafes.entrySet().stream()
+                .filter(entry -> entry.getValue().getCategories(categoryName).stream().anyMatch(category -> category.getName().equals(categoryName)))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse("Unknown Cafe");
+    }
+ 
+    public void processUserInput(String userInput) {
+        if (userInput != null && !userInput.isEmpty()) {
+            if (Features.validateInput(userInput)) {
+                List<String> completions = wordCompletion(userInput);
+                if (!completions.isEmpty()) {
+                    System.out.println("\nDid you mean: " + completions);
+                }
+                displayCategoriesWithPrices(userInput);
+            } else {
+                System.out.println("Invalid input. Please enter letters and spaces only.");
+            }
+        } else {
+            System.out.println("Input cannot be empty.");
+        }
+    }
 }
